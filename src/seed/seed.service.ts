600a0add1e88cbd5b3af21baca35fef4b4e398d8
Navau <milibrolunadepluton344@gmail.com>
2023-08-17 19:41:22 -0400

    17/08/23

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,21 +1,53 @@
-import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
+import {
+  Injectable,
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { map, size } from 'lodash';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
-import { forEach, size } from 'lodash';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
+  ) {}
   async executeSeed() {
-    const { data } = await axios.get<PokeResponse>(
+    const data = await this.http.get<PokeResponse>(
       `https://pokeapi.co/api/v2/pokemon?limit=650`,
     );
 
-    forEach(data.results, ({ name, url }) => {
+    // const insertPromisesArray = [];
+
+    const pokemons = map(data.results, ({ name, url }) => {
       const segments = url.split('/');
       const nro = Number(segments[size(segments) - 2]);
-      console.log(name, nro);
+      // insertPromisesArray.push(this.pokemonModel.create({ name, nro }));
+      return { name, nro };
     });
-    return data.results;
+
+    // await Promise.all(insertPromisesArray);
+
+    try {
+      await this.pokemonModel.deleteMany();
+      return await this.pokemonModel.insertMany(pokemons);
+    } catch (err) {
+      this.handleExceptions(err);
+    }
+  }
+  private handleExceptions(err: any) {
+    if (err.code === 11000)
+      throw new BadRequestException(
+        `Pokemon ${JSON.stringify(err.keyValue)} already exists in the DB`,
+      );
+    else
+      throw new InternalServerErrorException(
+        `Can't update Pokemon - Check server logs`,
+      );
   }
 }
